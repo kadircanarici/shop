eaea8987465337dfa6dfca899f848817985fb97a
kadircanarici <kadircanarc@gmail.com>
2023-08-26 16:13:15 +0300

    category modeli servis ile çağrıldı.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from './category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService
+  ) { }
   title = "Kategori Listesi"
-  categories: Category[] = [
-    { id: 1, name: "Bilgisayar" },
-    { id: 2, name: "Aksesuar" },
-    { id: 3, name: "Elektronik" }
-  ]
-
-  ngOnInit(): void {
+  categories!: Category[];
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data
+    })
   }
 
 }
